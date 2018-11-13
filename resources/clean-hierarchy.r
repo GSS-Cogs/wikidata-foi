2e2f413b83dc2dbfd30d48de479317734cfcb9ad
Robin Gower <robin.git@infonomics.ltd.uk>
2018-11-13 16:04:36 +0100

    Serialise csv correctly from R

--- resources/clean-hierarchy.r
+++ resources/clean-hierarchy.r
@@ -1,8 +1,9 @@
 library(dplyr)
-cc <- read.csv("cord-geographies-wikidata.csv")
+library(readr)
+cc <- read_csv("cord-geographies-wikidata.csv")
 
 # overwrite original, removing duplicate rows
-write.csv(cc %>% distinct(), "cord-geographies-wikidata.csv", row.names=F)
+write_csv(cc %>% distinct(), "cord-geographies-wikidata.csv")
 
 # overwrite original, removing duplicate rows
 write.csv(cc %>% distinct(Notation, .keep_all=T), "cord-geographies-single-parent.csv", row.names=F)
